6d662d4c2ae5ac2d3b2b3432fb6f64ed9d584852
assrat007 <asratadane169@gmail.com>
2023-08-26 18:28:01 +0300

    work sectiom finshied

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
     <div className='  bg-hero-pattern   bg-no-repeat  bg-cover  h-auto  '>
       <div className=' bg-gradient-to-r from-cyan-500/50 to-blue-500/50 h-screen'>
      <Nav/>
-     <section className='flex flex-col justify-center items-center mt-[60px]  '>
+     <section className='flex flex-col justify-center items-center p-4  '>
       <div className=' sm:w-[80%]'>
       <h1 className='text-[60px] sm:text-[100px] font-bold text-green-300 text-center  font-glutin '>Adwa films</h1>
      <p className=' hidden sm:block text-[28px] text-white font-semibold  text-justify  tracking-tighter sm:text-center  font-glutin'>Adwa films is an Ethiopian-based production company. Adwa films is a medium for sharing content which aims to present the best in Ethiopian movie to viewers throughout the world.. We broadcast feature films, documentaries, television shows, music videos, and short films through our YouTube account. additionally presents exclusive events like workshops and seminars 
@@ -20,16 +20,16 @@ function Hero() {
      <p className='block sm:hidden md:hidden lg:hidden xl:hidden text-center text-[23px] font-semibold text-white  font-glutin px-2'>Adwa films is an Ethiopian-based production company. Adwa films is a medium for sharing content which aims to present the best in Ethiopian movie to viewers throughout the world.. </p>
       </div>
       <div className='flex justify-center items-center mt-4 gap-x-4'>
-        <a href='#' className='w-24 h-24 sm:w-20 sm:h-20  rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
+        <a href='#' className='w-16 h-16  rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
 <        BsFacebook size={36}/>
         </a>
-        <a href='#' className='w-24 h-24 sm:w-20 sm:h-20 rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
+        <a href='#' className='w-16 h-16 rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
     <PiInstagramLogoFill size={36}/>
         </a>
-        <a href='#' className='w-24 h-24 sm:w-20 sm:h-20 rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
+        <a href='#' className='w-16 h-16 rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
 <AiFillYoutube size={36}/>
         </a>
-        <a href='#' className='w-24 h-24 sm:w-20 sm:h-20 rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
+        <a href='#' className='w-16 h-16 rounded-full border-[4px] border-green-300 text-white flex justify-center items-center'>
 <BsTelegram size={36}/>
         </a>
       </div>
@@ -38,7 +38,7 @@ function Hero() {
  
       </div>
       
-    
+    <div className='opacity-layer'></div>
       </div>
 
   )
--- src/componets/Work.jsx
+++ src/componets/Work.jsx
@@ -1,70 +1,64 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-import {AiOutlineArrowRight,AiOutlineArrowLeft} from 'react-icons/ai'
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { AiOutlinePlayCircle } from 'react-icons/ai';
+
+// Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import 'swiper/css/effect-cards';
+
+import './work.css';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
 import slide_image_1 from '../assets/sddefault.jpg';
 import slide_image_2 from '../assets/sddefault1.jpg';
 import slide_image_3 from '../assets/sddefault2.jpg';
 import slide_image_4 from '../assets/sddefault3.jpg';
 function Work() {
   return (
-    <>
-       <div className="block sm:hidden  bg-[#04152d]">
-      <p className=' text-[32px]  text-green-300 font-glutin font-semibold'>Our recent Uploads</p>
+    <section className='  bg-[#04152d] flex flex-col  sm:flex-row  justify-between items-center'>
+      <div className='flex justify-center items-center flex-col'>
+        <h1 className='text-[24px] sm:text-[40px] text-green-300 my-2 font-glutin font-bold'>our recent Uploads</h1>
       <Swiper
-        effect={'coverflow'}
+        effect={'cards'}
+        pagination={true}
         grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 200,
-          modifier: 1.2,
-        }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
+        modules={[EffectCards]}
+        className="mySwiper"
       >
         <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" />
+          <img src={slide_image_1} className='w-full h-1/2'/>
+          <a href='https://www.youtube.com/watch?v=XKDeHCBlGp0' target='_blank' className=' mt-6 animate-pulse '><AiOutlinePlayCircle className='w-[60px] h-[60px] text-white'/></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" />
+        <img src={slide_image_2} className='w-full h-1/2'/>
+          <a href='https://www.youtube.com/watch?v=6_At1wea2i0&t=6s' target='_blank' className=' mt-6 animate-pulse '><AiOutlinePlayCircle className='w-[60px] h-[60px] text-white'/></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" />
+        <img src={slide_image_3} className='w-full h-1/2'/>
+          <a href='https://www.youtube.com/watch?v=FbJVmYdFPpo' target='_blank' className=' mt-6 animate-pulse '><AiOutlinePlayCircle className='w-[60px] h-[60px] text-white'/></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide_image_4} alt="slide_image" />
+        <img src={slide_image_4} className='w-full h-1/2'/>
+          <a href='https://www.youtube.com/watch?v=UMKx2NLf17E&t=92s' target='_blank' className=' mt-6 animate-pulse '><AiOutlinePlayCircle className='w-[60px] h-[60px] text-white'/></a>
         </SwiperSlide>
-       
-        
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-           <AiOutlineArrowLeft/>
-          </div>
-          <div className="swiper-button-next slider-arrow">
-            <AiOutlineArrowRight/>
-          </div>
-          <div className="swiper-pagination"></div>
-        </div>
+      
       </Swiper>
+      </div>
+    
+    <div className="mt-4">
+<h1 className='text-[24px] sm:text-[40px] text-green-300 font-glutin font-semibold text-center sm:text-right'>We also promote Events </h1>
+<div className='px-6 py-4'>
+<p className='sm:text-[20px] text-white font-glutin text-center '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptas reiciendis reprehenderit omnis magnam alias accusamus! Quos aliquid quae quam ut assumenda deserunt sed beatae blanditiis, sit sint quis optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptas reiciendis reprehenderit omnis magnam alias accusamus! Quos ali</p>
+</div>
+<div className='flex  justify-center items-center'>
+<button className=' border-[3px] border-green-300 text-white rounded-full p-3 flex gap-x-2 items-center'>let's talk  business <AiOutlineArrowRight/></button>
+
+
+</div>
     </div>
-    <div className=' bg-[#04152d] h-screen'>
-     <h1 className='text-[40px] hidden sm:block text-green-300 '>Recent Uploads</h1>
-    </div>
-    </>
+    </section>
    
     
   )
